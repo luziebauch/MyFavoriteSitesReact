7ae9e358e1a45640985a204c52e4841fd05c3fc1
Luzie Bauch <luzie.bauch@tobit.com>
2020-09-17 15:21:40 +0200

    :construction: Fetch data in own file

--- src/api/todos/get.js
+++ src/api/todos/get.js
@@ -9,3 +9,18 @@ export const getTodosFromLocalStorage = async () => new Promise((resolve) => {
         resolve({ status: 200, data: JSON.parse('[]') });
     }
 });
+
+export const getChaynsSites = async (skip, searchString = 'Ahaus', search, setArrayStart) => {
+    chayns.showWaitCursor();
+    let data;
+    if (searchString !== '') {
+        data = await fetch(`https://chayns2.tobit.com/SiteSearchApi/location/search/${searchString}/?skip=${skip}&take=25`);
+    } else {
+        data = await fetch(`https://chayns2.tobit.com/SiteSearchApi/location/search/${search}/?skip=${skip}&take=25`);
+    }
+    const arrayData = await data.json();
+    if (arrayData !== null) {
+        setArrayStart((lastArray) => lastArray.concat(arrayData));
+    }
+    chayns.hideWaitCursor();
+};
